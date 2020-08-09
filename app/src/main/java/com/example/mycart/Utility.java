217968453a1568ee520515d31803c61070f33475
mvreddy30 <iam.venkatreddie@gmail.com>
2020-08-08 22:03:53 -0400

    fifth commit

---
+++ app/src/main/java/com/example/mycart/Utility.java
@@ -0,0 +1,10 @@
+package com.example.mycart;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Utility {
+    public static boolean LoginSymbol=false;
+
+    public static List<WishlistProduct> products = new ArrayList<>();
+}
